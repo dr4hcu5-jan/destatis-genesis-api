026cad06a3177679b8ccd04ce3fc79edb43337d9
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-02-25 09:29:27 +0100

    Added helper function for testing if a host is available on a specified port

---
+++ src/tools/__init__.py
@@ -0,0 +1,34 @@
+"""A collection of tools which are used and needed multiple times in this project"""
+import asyncio
+import time
+
+from .. import MODULE_LOGGER
+
+
+async def is_host_available(
+        host: str,
+        port: int,
+        timeout: int
+) -> bool:
+    """Check if the specified host is reachable on the specified port
+
+    :param host: The hostname or ip address which shall be checked
+    :param port: The port which shall be checked
+    :param timeout: Max. duration of the check
+    :return: A boolean indicating the status
+    """
+    _end_time = time.time() + timeout
+    while time.time() < timeout:
+        try:
+            # Try to open a connection to the specified host and port and wait a maximum time of five seconds
+            _s_reader, _s_writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=5)
+            # Close the stream writer again
+            _s_writer.close()
+            # Wait until the writer is closed
+            await _s_writer.wait_closed()
+            return True
+        except:
+            # Since the connection could not be opened wait 5 seconds before trying again
+            await asyncio.sleep(5)
+    return False
+
