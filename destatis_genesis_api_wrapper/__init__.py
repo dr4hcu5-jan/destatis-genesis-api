53afd1c88890eab67d3f4a35814a5f53980076b5
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-03-04 11:09:47 +0100

    Added the tables2variable method from the documentation

--- destatis_genesis_api_wrapper/__init__.py
+++ destatis_genesis_api_wrapper/__init__.py
@@ -538,4 +538,35 @@ class AsyncGENESISWrapper:
                 _url, _param, Catalogue.TableResponse
             )
 
-        
+        async def tables_to_variable(
+                self,
+                variable_name: str,
+                table_selector: str = None,
+                object_area: GENESISArea = GENESISArea.ALL,
+                result_count: int = 100
+        ) -> Catalogue.TableResponse:
+            """Get a list of tables matching the table selector which are assigned to the
+
+            :param variable_name: Name of the statistic [required, 1-15 characters]
+            :param table_selector: Filter for the tables code [optional, wildcards allowed]
+            :param object_area: The location of the statistic/tables
+            :param result_count: The number of tables in the response
+            :return:
+            """
+            if variable_name is None:
+                raise ValueError('The name of the statistic is required to get the tables')
+            if not 1 <= len(variable_name) <= 15:
+                raise ValueError('The length of the statistics name needs to be between 1 and 15')
+            if (table_selector is not None) and not (1 <= len(table_selector) <= 15):
+                raise ValueError('The table selector needs to be at least 1 character and max 15 '
+                                 'characters')
+            _param = self.__base_parameter | {
+                'name':       variable_name,
+                'selection':  table_selector,
+                'area':       object_area.value,
+                'pagelength': result_count
+            }
+            _url = self._service_url + '/tables2variable'
+            return await tools.get_parsed_response(
+                _url, _param, Catalogue.TableResponse
+            )
